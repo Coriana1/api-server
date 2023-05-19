a8faab288a38e55ef1ed4808e4b8d4b3b4f56666
Coriana Williams <corianacory@gmail.com>
2023-05-18 22:46:29 -0400

    created branch

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,9 @@
+'use strict';
+
 const { Sequelize, DataTypes } = require('sequelize');
+const customer = require('./customer');
+// const order = require('./order');
+// const Collection = require('./collection');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test' ? 'sqlite::memory:' : process.env.DATABASE_URL;
 
@@ -13,6 +18,8 @@ const ClothesModel = require('./clothes');
 // Initialize the models
 const Food = FoodModel(sequelize, DataTypes); //lowercase 
 const Clothes = ClothesModel(sequelize, DataTypes);
+
+//connecting database
 
 
 module.exports = {
